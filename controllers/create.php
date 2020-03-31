dd90b092cf99c0bda19f63e5daf6eb6a6f49df69
Hermoult <adrienhergat@gmail.com>
2020-03-31 15:29:12 +0200

    modif controllers

--- controllers/create.php
+++ controllers/create.php
@@ -1,31 +1,26 @@
 <?php
 
     // Recuperation des données entrée par le client, mise en variable
-
     $mail = htmlspecialchars($_POST['mail']);
     $mdp = htmlspecialchars($_POST['mdp']);
 
+    require ('log.php'); // Recupère les logs accès BDD
 
-    // Variables Database
-    $DB_NAME = "worldofface";
-    $DB_DSN = "mysql:host=localhost;dbname=".$DB_NAME;
-    $DB_USER = "adrien"; 
-    $DB_PASSWORD = "adrien"; 
-
-    // Connexion Database
-    try {
-        $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (\Throwable $th) {
-        die('error sql connection');
-    } 
-
-    // Preparation et execution de la commande sql
-
-    $sql = "INSERT INTO user (mail, mdp) VALUES (?,?)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$mail,$mdp,]);
+    // Test de l'existance du profil
+    $sqls = 'SELECT * FROM user WHERE mail = ?';
+    $stmt = $pdo->prepare($sqls);
+    $stmt->execute([$mail]);
     $d = $stmt->fetch();
-    if($mail == $d['mail'] && $mdp == $d['mdp']){
 
-    header('location: ../commande.html');
+    
+    if($mail == $d['mail']){
+        echo "Adresse mail existante";
+    }
+    else{
+        // Creation profil et accès gallerie
+        $sqli = "INSERT INTO user (mail, mdp) VALUES (?,?)";
+        $stmt = $pdo->prepare($sqli);
+        $stmt->execute([$mail,$mdp]);
+        echo"nouveau profil créé";
+        /* header('location: ../commande.html'); */
+        }
---
+++ controllers/log.php
@@ -0,0 +1,15 @@
+<?php
+    // Variables Database
+    $DB_NAME = "worldofface";
+    $DB_DSN = "mysql:host=localhost;dbname=".$DB_NAME;
+    $DB_USER = "adrien"; 
+    $DB_PASSWORD = "adrien"; 
+
+    // Connexion Database
+    try {
+        $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (\Throwable $th) {
+        die('error sql connection');
+    } 
+    ?>
